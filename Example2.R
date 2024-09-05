972a21929cdea870e1fdfc50453b1c9c4284e4d7
Devon Maywald <dmaywald@tamu.edu>
2024-09-05 09:54:28 -0500

    Finish classify_for and classify_vec in Example2.R

--- Example2.R
+++ Example2.R
@@ -7,38 +7,70 @@ require(mnormt) # for multivariate normal data generation
 # beta - supplied discriminant vector
 # xtrain, ytrain - training data
 # xtest, ytest - testing data
+# xtrain and xtest are matrices with p columns,
+# ytrain and ytest are vectors; labels are 1 and 2
 #
 # OUTPUT
 # ypred - predicted class membership
 # error - percent of misclassified observations
 
-classify_for <- function(beta, xtrain, ytrain, xtest, ytest){
+classify_for <- function(beta, xtrain, ytrain, xtest, ytest) {
   # [ToDo] Code discriminant analysis classifier using for loop
   
   # Calculate sample means based on training data
-  
+  xbar1 <- colMeans(xtrain[ytrain == 1, ])
+  xbar2 <- colMeans(xtrain[ytrain == 2, ])
   
   # Calculate class assignments for xtest in a for loop
+  npred <- length(ytest)
+  ypred <- rep(1, npred)
   
+  
+  for (i in 1:npred) {
+    # use h(x) rule to predict
+    #((xtest[i, ] - xbar)^T %*% beta)^2
+    h1 <- as.numeric((crossprod((xtest[i, ] - xbar1), beta)) ^ 2)
+    
+    h2 <- as.numeric((crossprod((xtest[i, ] - xbar2), beta)) ^ 2)
+    
+    if (h2 < h1) {
+      ypred[i] <- 2
+    }
+  }
   # Calculate % error using ytest
+  error <- 100*mean(ypred != ytest)
   
   # Return predictions and error
   return(list(ypred = ypred, error = error))
 }
+
 
 classify_vec <- function(beta, xtrain, ytrain, xtest, ytest){
   # [ToDo] Try to create vectorized version of classify_for
   
   # Calculate sample means based on training data
+  xbar1 <- colMeans(xtrain[ytrain == 1, ]) # drop= FALSE
+  xbar2 <- colMeans(xtrain[ytrain == 2, ])
+  
+  # Calculate the inner product of the means with beta
+  m1b <- as.numeric(crossprod(xbar1, beta))
+  m2b <- as.numeric(crossprod(xbar2, beta))
+  
+  # Calculate produce of xtest with beta
+  xtestb <- xtest%*%beta
   
   # Calculate class assignments for xtest using matrix and vector algebra
+  h1 <- (xtestb - m1b)^2
+  h2 <- (xtestb - m2b)^2
+  
+  ypred <- as.numeric((h2 < h1) + 1) # ifelse()
   
   # Calculate % error using ytest
+  error <- 100*mean(ypred != ytest)
   
   # Return predictions and error
   return(list(ypred = ypred, error = error))
 }
-
 # Example 
 ##############################################
 
@@ -80,7 +112,13 @@ out1 = classify_for(beta, xtrain, ytrain, xtest, ytest)
 out2 = classify_vec(beta, xtrain, ytrain, xtest, ytest)
 
 # [ToDo] Verify the assignments agree with each other
+testthat::expect_equal(out1,out2)
+summary(out1$ypred - out2$ypred)
 
 # [ToDo] Use microbenchmark package to compare the timing
 
 library(microbenchmark)
+microbenchmark(
+  classify_for(beta, xtrain, ytrain, xtest, ytest),
+  classify_vec(beta, xtrain, ytrain, xtest, ytest)
+)
